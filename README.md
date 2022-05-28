a1c536bf8863fca3f022110dd30f58f73cb9995e
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-28 17:46:53 -0400

    local storage pull works. need to colorize time blocks. currenthour is set to test mode

---
+++ README.md
@@ -0,0 +1,54 @@
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+
+# Smart Planner Scheduler
+
+## Description
+WordWise Trivia Game will test your knowledge of Javascript. Answer multiple choice questions before the time runs out... but beware, if you get the answer wrong, you will lose time! 
+
+## Installation
+
+Follow these steps to install this project on your local machine.
+
+- Open Bash editor
+- mkdir dennislittlefieldportfolio
+- cd dennislittlefieldportfolio
+- git clone https://github.com/DLittlefield81/WordWise.git
+
+## Application
+
+Link to Deployed Application: https://dlittlefield81.github.io/WordWise/
+
+
+![WordWise Trivia Game](assets/images/wordwise.png)
+![Wireframe](assets/images/wireframe.png)
+
+
+## License
+
+Non-Software Licenses
+Open source software licenses can be also used for non-software works and are often the best choice, especially when the works in question can be edited and versioned as source (e.g., open source hardware designs). Choose an open source license here.
+
+Data, media, etc.
+CC0-1.0, CC-BY-4.0, and CC-BY-SA-4.0 are open licenses used for non-software material ranging from datasets to videos. Note that CC-BY-4.0 and CC-BY-SA-4.0 should not be used for software.
+
+Documentation
+Any open source software license or open license for media (see above) also applies to software documentation. If you use different licenses for your software and its documentation, be sure to specify that source code examples in the documentation are also licensed under the software license.
+
+Fonts
+The SIL Open Font License 1.1 keeps fonts open, allowing them to be freely used in other works.
+
+Mixed projects
+If your project contains a mix of software and other material, you can include multiple licenses, as long as you are explicit about which license applies to each part of the project. See the license notice for this site as an example.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,77 +5,41 @@ setInterval(function () {
 });
 
 
+
+
+//============- Set to LocalStorage -======================
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+})
+
+
+
 //============- Get Current Hour -======================
 let currentHour = 13; //moment().format('HH');
 console.log("Current Hour: " + currentHour)
 
 
-//============- Build Table -======================
-// function createTables() {
-   
-
-//     for (var i = 9; i <= 17; i++) {
-//         //console.log(i)
-
-//         var createDivRow = document.createElement('div');
-//         createDivRow.className = 'row time-block';
-//         createDivRow.id = 'hour-' + i;
-//         this.appendChild(createDivRow);
-//     }
-    
-//     for (var i = 9; i <= 17; i++) {
-//         var createDivHour = document.createElement('div');
-//         createDivRow.className = 'col-md-1 hour';
-//         createDivHour.id = 'hour';
-//         toAdd.appendChild(createDivHour);
-//     }
-// document.getElementById('container').appendChild(toAdd);
-//      document.getElementById('hour').appendChild(toAdd);
-// };
-// 
-// createDivHour.innerHTML = i;
-// createDivHour.className = 'col-md-1 hour';
-// var createTextArea = document.createElement('textarea');
-// var createBtnSave = document.createElement('button');
-
-// 
-// toAdd.appendChild(createTextArea);
 
 
+//============- Get Local Storage -======================
+function getLocalStorage() {
+    for (var i = 9; i <= 17; i++) {
 
+        $("#hour" + [i] + " .description").val(localStorage.getItem("hour" + [i]));
 
-//document.getElementById('hour-' + i).appendChild(createBtnSave);
-
-
-
-
-
-// };
-
-/*
-
-    <div id = "hour-9" class = "row time-block" >
-        <div class = "col-md-1 hour" >9 AM </div> 
-        <textarea class = "col-md-10 description"> </textarea> 
-        <button class = "btn saveBtn col-md-1" ><i class = "fa-solid fa-lock" >< /i> </button >
-    </div> 
-    
-    */
-
-
-
-//var element = document.querySelector(".site3");
-
-//============- Colourize Table -======================
-for (var i = 9; i <= currentHour - 1; i++) {
-    console.log("P:" + i)
-    // element.classList.add("past");
-}
-for (var x = 17; x >= currentHour + 1; x--) {
-    console.log("F:" + x)
-    //element.classList.add("future");
+    }
 }
 
 
-//============- ON LOAD -======================
-//createTables()
+
+getLocalStorage();
+
+
